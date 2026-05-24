b4aa229f14643583c57e98858f87b0aa67b86112
belemone <126007365+belemon81@users.noreply.github.com>
2026-05-25 01:25:37 +0700

    Update resources.ts

--- trangdq-1/PRO Bookstore/BookStore_FE_Angular/src/app/shared/resources.ts
+++ trangdq-1/PRO Bookstore/BookStore_FE_Angular/src/app/shared/resources.ts
@@ -3,9 +3,9 @@ import {Subject} from "rxjs";
 
 export const SERVER_URL = "https://internship-p5ci.onrender.com"
 
-export const PASSWORD_PATTERN = /^[A-Z][a-zA-Z0-9]{7}$/;
+export const PASSWORD_PATTERN = /^[A-Z][a-zA-Z0-9]$/;
 
-export const EMAIL_PATTERN = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/
+export const EMAIL_PATTERN = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]$/
 
 export const PHONE_PATTERN = /^[0-9]{10}$/;
 
